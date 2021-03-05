f14a68cd20be2dbc41209f4340e269d4503e3a5e
vagiakar <vagiakaraba@gmail.com>
2021-03-05 18:38:49 +0200

    feat:delete ships when delete button is clicked and appear start game button when all ships are placed

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const strategyPlayerGridItems = [
   ...document.querySelectorAll("[data-strategy-player-grid-item]"),
 ];
 const rotateBtn = document.querySelector("#rotate-btn");
+const startGameBtn = document.querySelector("#start-game-btn");
+const deleteAllBtn = document.querySelector("#delete-all-btn");
+const shipsGrid = document.querySelector("#ships-grid");
+const ships = [...document.querySelectorAll("[data-ship-name]")];
 let dragoverRows = null;
 let dragoverColumns = null;
 let dragoverIndexes = null;
@@ -18,11 +22,20 @@ let playerGridData = [];
 
 btnVSComputer.addEventListener("click", handleStrategy);
 
+startGameBtn.addEventListener("click", () => {
+  handleGame();
+});
+
 function handleStrategy() {
   initialisePlayerGridData();
   displayStrategySection();
   handleDragAndDropShips();
   handleRotate();
+  handleDeleteAll();
+}
+
+function handleGame() {
+  console.log(playerGridData);
 }
 
 function initialisePlayerGridData() {
@@ -41,7 +54,7 @@ function displayStrategySection() {
 function handleDragAndDropShips() {
   let clickOffsetX;
   let clickOffsetY;
-  const ships = [...document.querySelectorAll("[data-ship-length]")];
+
   ships.forEach((ship) => {
     ship.addEventListener("dragstart", (e) => {
       [clickOffsetX, clickOffsetY] = dragStart(e, clickOffsetX, clickOffsetY);
@@ -73,6 +86,23 @@ function dragEnd(e) {
   e.target.classList.remove("dragging");
   strategyPlayerGridItems.forEach((item) => {
     item.classList.remove("dragover");
+  });
+  addOrRemoveStartBtn();
+}
+
+function addOrRemoveStartBtn() {
+  if (isEveryShipOnGrid()) {
+    startGameBtn.classList.remove("visibility-hidden");
+  } else {
+    startGameBtn.classList.add("visibility-hidden");
+  }
+}
+
+function isEveryShipOnGrid() {
+  return ships.every((ship) => {
+    return playerGridData.some((item) => {
+      return ship.dataset.shipName === item.placed;
+    });
   });
 }
 
@@ -249,9 +279,9 @@ function addGridItems() {
 }
 
 function removeStrategyPlaced() {
-  strategyPlayerGridItems.forEach((item, index) => {
-    if (playerGridData[index].placed === dragging.classList[1]) {
-      playerGridData[index].placed = null;
+  playerGridData.forEach((item) => {
+    if (item.placed === dragging.classList[1]) {
+      item.placed = null;
     }
   });
 }
@@ -396,4 +426,20 @@ function getDragoverIndexesFromRowsAndColumns(
   return newDragoverIndexes;
 }
 
-function clearAll() {}
+function handleDeleteAll() {
+  deleteAllBtn.addEventListener("click", () => {
+    ships.forEach((ship) => {
+      ship.style.gridRow = ``;
+      ship.style.gridColumn = ``;
+      dragging = null;
+      playerGridData.forEach((item) => {
+        item.placed = null;
+      });
+      strategyPlayerGridItems.forEach((item) => {
+        item.classList.remove("display-none");
+      });
+      shipsGrid.appendChild(ship);
+      startGameBtn.classList.add("visibility-hidden");
+    });
+  });
+}
